b4545fa5c1d2d20b7a6c0d959756da94c99aafa8
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-03-11 11:51:41 -0300

    feat: Card Earnings added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RiLineChartLine } from 'react-icons/ri';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
@@ -5,9 +6,19 @@ function App() {
   return (
     <div className='grid lg:grid-cols-4 xl:grid-cols-6 min-h-screen'>
       <Sidebar />
-      <div className='lg:col-span-3 xl:col-span-5 bg-gray-100 p-8'>
+      <main className='lg:col-span-3 xl:col-span-5 bg-gray-100 p-8'>
         <Header />
-      </div>
+        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10'>
+          <div className='bg-primary-100/90 p-8 rounded-xl text-gray-300 flex flex-col gap-4'>
+            <RiLineChartLine className='text-5xl' />
+            <h4 className='text-2xl'>Earnings</h4>
+            <span className='text-5xl text-white'>&euro; 8.350</span>
+            <span className='py-1 px-3 bg-primary-300/80 rounded-full'>
+              + 10% since last month
+            </span>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
