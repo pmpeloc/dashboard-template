5fea4a3a41a36ddc5b41395bc817bc276a581350
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-03-11 15:20:58 -0300

    feat: Recent invoices and CTA added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RiLineChartLine } from 'react-icons/ri';
+import { RiHashtag, RiLineChartLine } from 'react-icons/ri';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='grid lg:grid-cols-4 xl:grid-cols-6 min-h-screen'>
       <Sidebar />
-      <main className='lg:col-span-3 xl:col-span-5 bg-gray-100 p-8'>
+      <main className='lg:col-span-3 xl:col-span-5 bg-gray-100 p-8 h-[100vh] overflow-y-scroll'>
         <Header />
         {/* Section 1 */}
         <section className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10 gap-8'>
@@ -65,7 +65,7 @@ function App() {
                   <p className='text-gray-500'>1 day remaining</p>
                 </div>
               </div>
-              <div className='flex items-center gap-4'>
+              <div className='flex items-center gap-4 mb-4'>
                 <img
                   src='https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg'
                   alt='User'
@@ -76,10 +76,78 @@ function App() {
                   <p className='text-gray-500'>1 day remaining</p>
                 </div>
               </div>
+              <div className='flex justify-end'>
+                <a
+                  href='#'
+                  className='hover:text-primary-100 transition-colors hover:underline'>
+                  See all projects
+                </a>
+              </div>
             </div>
           </div>
         </section>
         {/* Section 2 */}
+        <section className='grid grid-cols-1 md:grid-cols-2 mt-10 gap-8'>
+          <div>
+            <h1 className='text-2xl font-bold mb-8'>Recent invoices</h1>
+            <div className='bg-white p-8 rounded-xl shadow-2xl mb-8'>
+              {/* Card 1 */}
+              <div className='grid grid-cols-4 items-center gap-4 mb-4'>
+                <div className='col-span-2 flex items-center gap-4'>
+                  <img
+                    src='https://img.freepik.com/foto-gratis/joven-confiado_1098-20868.jpg'
+                    alt='User'
+                    className='w-14 h-14 object-cover rounded-xl'
+                  />
+                  <div>
+                    <h3 className='font-bold'>Alexander Williams</h3>
+                    <p className='text-gray-500'>Tech Lead</p>
+                  </div>
+                </div>
+                <div>
+                  <span className='bg-green-100 text-green-800 py-1 px-3 rounded-full font-medium'>
+                    Paid
+                  </span>
+                </div>
+                <span className='font-bold'>&euro; 1.200,87</span>
+              </div>
+              {/* Card 2 */}
+              <div className='grid grid-cols-4 items-center gap-4 mb-4'>
+                <div className='col-span-2 flex items-center gap-4'>
+                  <img
+                    src='https://img.freepik.com/fotos-premium/feliz-alegre-joven-empresario-morena-brazos-cruzados-mirando-al-frente-sonrisa-alegre-encantadora_132358-1597.jpg'
+                    alt='User'
+                    className='w-14 h-14 object-cover rounded-xl'
+                  />
+                  <div>
+                    <h3 className='font-bold'>John Philips</h3>
+                    <p className='text-gray-500'>Inchor Techs</p>
+                  </div>
+                </div>
+                <div>
+                  <span className='bg-red-100 text-red-800 py-1 px-3 rounded-full font-medium'>
+                    Late
+                  </span>
+                </div>
+                <span className='font-bold'>&euro; 12.989,87</span>
+              </div>
+            </div>
+            <div className='bg-primary-900 text-gray-300 p-8 rounded-xl shadow-2xl flex items-center justify-between'>
+              <div className='flex gap-4 items-center'>
+                <RiHashtag className='text-4xl -rotate-12' />
+                <div>
+                  <h5 className='font-bold text-white'>Engage with clients</h5>
+                  <h5>Join Slack channel</h5>
+                </div>
+              </div>
+              <div>
+                <button className='bg-primary-100 py-2 px-6 rounded-xl text-white'>
+                  Join now
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
